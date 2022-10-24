bd7f53428ed8625472f8e17507089a003c4ab19d
Marley Judd <marleymaejudd@gmail.com>
2022-10-24 12:47:40 -0600

    starting to get the api call to work

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/books')
+      return response
+    }
+    console.log(apiCall())
+  }, [])
+
   return (
     <div className="App">
       <h1>Biased Book Reviews</h1>
