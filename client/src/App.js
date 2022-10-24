f824e4e3fd2b903165c427d593f4aa19bd8a2d62
Marley Judd <marleymaejudd@gmail.com>
2022-10-24 14:13:30 -0600

    made components directory and some components jsx

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,24 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [books, updateBooks] = useState([])
+
   useEffect(() => {
     const apiCall = async () => {
       let response = await axios.get('http://localhost:3001/books')
-      return response
+      updateBooks(response.data.allBooks)
     }
-    console.log(apiCall())
+    apiCall()
   }, [])
 
   return (
     <div className="App">
       <h1>Biased Book Reviews</h1>
+      {books.map((book) => (
+        <div>
+          <h2>{book.title}</h2>
+        </div>
+      ))}
     </div>
   )
 }
