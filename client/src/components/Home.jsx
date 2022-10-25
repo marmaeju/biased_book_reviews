813d7bf4fc4432ba1a24e88f83534402469517bb
Marley Judd <marleymaejudd@gmail.com>
2022-10-25 11:45:37 -0600

    added formState to inputs in form

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Home = (props) => {
+  const [formState, setFormState] = useState({ title: "", author: "", description: "", image: ""})
 
   let navigate = useNavigate()
 
@@ -21,13 +22,13 @@ const Home = (props) => {
       <h3>Don't see the book you want? Create it in the form below!</h3>
       <form>
         <label htmlFor='title'>Title:</label>
-        <input id='title'/>
+        <input id='title' value={formState.title}/>
         <label htmlFor='author'>Author:</label>
-        <input id='author'/>
+        <input id='author' value={formState.author}/>
         <label htmlFor='description'>Description:</label>
-        <input id='description'/>
+        <input id='description' value={formState.description}/>
         <label htmlFor='image'>Image URL:</label>
-        <input id='image'/>
+        <input id='image' value={formState.image}/>
         <button type='submit'>Add Book!</button>
       </form>
 
