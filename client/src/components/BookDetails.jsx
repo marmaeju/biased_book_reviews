88adc0cd2fbbe87f52dafec511aa1955501edefc
Marley Judd <marleymaejudd@gmail.com>
2022-10-27 07:56:44 -0600

    fixed rendering reviews .map function

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 const BookDetails = (props) => {
   // const [formState, setFormState] = useState({ title: "", body: "", name: ""})
   const [book, setBook] = useState({})
+  const [reviews, setReviews] = useState([])
   let {id} = useParams()
 
   // const handleChange = (event) => {
@@ -30,10 +31,12 @@ const BookDetails = (props) => {
     const getBook = async () => {
       let response = await axios.get(`http://localhost:3001/books/${id}`)
       setBook(response.data)
+      setReviews(response.data.reviews)
+      console.log(book, "useEffect")
     }
     getBook()
-  }, [props.books, id])
-
+  }, [id])
+console.log(book)
   return (
     <div className="book-details">
       <div>
@@ -43,16 +46,16 @@ const BookDetails = (props) => {
         <h3 className="description">Description</h3>
         <h4>{book.description}</h4>
       </div>
-      {/* <div className="review">
+      <div className="review">
         <h3 className="reviews-title">Reviews</h3> 
-        {book?book.reviews.map((review) => (
+        {reviews ? reviews.map((review) => (
           <div>
             <h4>{review.title}</h4>
-            <h3>{review.body}</h3>
+            <h4>{review.body}</h4>
             <h5>~{review.name}</h5>
           </div>
         )): "" }
-      </div> */}
+      </div>
       <div>
       {/* <div className='review-form'>
         <h2>Want to add your own review? Create it in the form below!</h2>
