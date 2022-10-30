181f3c195c03453b170624b673fe4b268dd0a589
Marley Judd <marleymaejudd@gmail.com>
2022-10-30 13:54:53 -0600

    Updated README and added a tiny quip to the about page.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,6 +3,7 @@ const About = () => {
     <div>
       <div className="aboutH1">
       <h1>Read Biased Reviews of Your Favorite Books!</h1>
+      <h5>That's all there is to know about this web page. Snazzy, I know.</h5>
       </div>
     </div>
   )
